b372284bcfb08118d1df3ca0aa0d31bb9cd826fc
DrWallie <WallieWallex@hotmail.com>
2016-02-05 10:43:46 +0100

    Big Update #1
    big update

---
+++ Main files/Multiplayer FPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+	
+	[SerializeField]
+	private float thrusterForce = 1000f;
+
+	private PlayerMotor motor;
+
+	void Start ()
+	{
+		motor = GetComponent<PlayerMotor>();
+	}
+	
+	//Calculate the thruster force based on player input.
+	public void Jetpack ()
+	{
+		Vector3 _thrusterForce = Vector3.zero;
+		//Apply thruster force
+		if (Input.GetButton ("Jump")) 
+		{
+			_thrusterForce = Vector3.up * thrusterForce;
+		}
+
+		motor.Jetpack (_thrusterForce);
+	}
+}
---
+++ Main files/Multiplayer FPS/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	private Vector3 thrusterForce = Vector3.zero;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+
+	//Get a force vector for the Jetpack
+	public void Jetpack (Vector3 _thrusterForce)
+	{
+		thrusterForce = _thrusterForce;
+	}
+
+	//Run every physics iteration
+	void FixedUpdate ()
+	{
+		PerformMovement ();
+	}
+
+	//Perform movement based on velocity variable
+	void PerformMovement()
+	{
+		if (thrusterForce != Vector3.zero) 
+		{
+			rb.AddForce(thrusterForce * Time.deltaTime, ForceMode.Acceleration);
+		}
+	}
+}
